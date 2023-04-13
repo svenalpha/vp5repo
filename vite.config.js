822d0bcf60726ea427c0efe866cbb200a81c05ab
svenalpha <svalphaforsale@yahoo.com>
2023-04-13 19:34:08 +0700

    4th push

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+import handlebars from 'vite-plugin-handlebars';
+//import { resolve } from 'path';
+import path from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+     base: '/vp5/'
+  })
+  
+
+
+
+
