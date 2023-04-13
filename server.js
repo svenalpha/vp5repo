c24395942d1592f80e5af93864f55d8fbaab6b6d
svenalpha <svalphaforsale@yahoo.com>
2023-04-13 22:23:04 +0700

    10th push

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
   //console.log(" procss.env = ",process.env);
 //console.log("import.meta.env",import.meta.env);
 
-app.get("/message", (_, res) => res.send("Hello from  hgtgfhhxxx express!"));
+app.get("/message", (_, res) => res.send("Hello from  aaahgtgfhhxxx express!"));
 
 ViteExpress.listen(app, 3000, () => console.log("Server is listening..."));
 
