f771c1fa0d0eb8c2420b049443a7fdd53f0a898b
svenalpha <svalphaforsale@yahoo.com>
2023-04-14 16:22:23 +0700

    with css, 15th push of 14/4/2023

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import path from "path";
 const app = express();
 
 //    ViteExpress.config({ mode: "development"});  //"production" })  //  test for preview, development by default
-    ViteExpress.config({ mode: "production"});   // NODE_ENV=production node server.ts     NODE_ENV=development node server.ts
+ //       ViteExpress.config({ mode: "production"});   // NODE_ENV=production node server.ts     NODE_ENV=development node server.ts
  //  ViteExpress.config({ mode: "test"}); 
   //console.log(" procss.env = ",process.env);
 //console.log("import.meta.env",import.meta.env);
