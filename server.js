e7a5996c3946d317f9d209d7aa4fd5bc340072a7
svenalpha <svalphaforsale@yahoo.com>
2023-04-14 14:44:56 +0700

    14th push of 14/4/2023

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 
 
 import express from "express";
-import ViteExpress from "vite-express";
+import ViteExpress from "vite-express";  
+import path from "path";
 
 const app = express();
 
@@ -11,8 +12,14 @@ const app = express();
   //console.log(" procss.env = ",process.env);
 //console.log("import.meta.env",import.meta.env);
 
+//console.log(" __dirname = ",__dirname);
 app.get("/", (_, res) => res.send("Hello from  home"));
-app.get("/message", (_, res) => res.send("Hello from  aaagfhhxxx express!"));
+//app.get("/about", (_, res) => {res.sendFile("c:/web/a5/vp5/about.html");});  
+var __dirname = path.resolve(path.dirname('')); 
+app.get("/about", (_, res) => {res.sendFile("./about.html",{root: __dirname});}); 
+
+
+app.get("/contact", (_, res) => res.send("Hello from  contact express!")); 
 app.get("*", (_, res) => res.send("Hello from  404 404 404"));
 
 ViteExpress.listen(app, 3000, () => console.log("Server is listening..."));
